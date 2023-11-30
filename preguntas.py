c70449cdff178af989c90ecba21a0ec14d77f26c
jomurielm <144398722+jomurielm@users.noreply.github.com>
2023-11-30 17:34:43 -0500

    Desarrollo del laboratorio propuesto

--- preguntas.py
+++ preguntas.py
@@ -20,7 +20,7 @@ def pregunta_01():
     digits = datasets.load_digits()
 
     # Imprima los nombres de la variable target del dataset
-    print(digits.target)
+    print(digits.target_names)
 
     # Imprima las dimensinoes de matriz de datos
     print(digits.data.shape)
